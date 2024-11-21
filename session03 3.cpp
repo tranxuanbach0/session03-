8bdf9333eda9395852057b7655b984ad6891091e
tranxuanbach0 <trantiger706@gmail.com>
2024-11-21 21:41:32 +0700

    session03 3

---
+++ session03 3.cpp
@@ -0,0 +1,29 @@
+ #include <stdio.h>
+
+int main() {
+    float r, chuVi, dienTich;
+    const float PI = 3.14159;
+
+    printf("Nhap ban kinh hinh tron (r): ");
+    scanf("%f", &r);
+
+    chuVi = 2 * PI * r;
+    dienTich = PI * r * r;
+
+    printf("Chu vi hinh tron: %.2f\n", chuVi);
+    printf("Dien tich hinh tron: %.2f\n", dienTich);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
